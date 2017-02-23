94e04a08325546fd86e93e3941de50a64cbfff10
Po Buchi <po@ox2.io>
2017-02-23 14:16:30 +0700

    removed client code

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:login',
-  version: '0.2.0',
+  version: '0.3.0',
   // Brief, one-line summary of the package.
   summary: 'TEST, DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -21,21 +21,16 @@ Package.onUse(function(api) {
       'ecmascript',
       'mongo',
       'promise',
-      'templating',
-      'less',
       'check',
       'random',
       'email',
     ]);
     // 3rd party
     api.use([
-      'mquandalle:jade@0.4.9', 'ox2:inject-style@1.0.0', 'tapfuse:collection-global@2.0.0',
+      'tapfuse:collection-global@2.0.0',
     ]);
     api.addFiles('lib/lib/oo-login-collection.js', CS);
     api.addFiles('lib/oo-login-server.js', S);
-    api.addFiles('lib/oo-login-client.less', C);
-    api.addFiles('lib/oo-login-client.jade', C);
-    api.addFiles('lib/oo-login-client.js', C);
     api.export('validateEmail', CS);
 });
 
